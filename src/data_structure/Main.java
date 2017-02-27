bf2a51ca346607a37471bdf1e7c232a1ef529409
quitegws <quitegws@gmail.com>
2017-02-27 21:11:40 +0800

    add source files

---
+++ src/data_structure/Main.java
@@ -0,0 +1,21 @@
+package data_structure;
+ 
+public class Main{
+	public static void main(String args[]){
+		for(int i=0; i<10; i++){
+			System.out.println("第"+ i + "个斐波那契数是：" + fun(i) );
+		}
+	}
+	
+	public static long fun(long num){
+		long result = 0;
+		
+		if(num == 0 || num == 1){
+			result = num;
+		}else{
+			result = fun(num - 1) + fun(num - 2);
+		}
+		
+		return result;
+	}
+}
