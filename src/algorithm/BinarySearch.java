868868b65fff6137b7c5e5b4be6700aa253b770c
quitegws <quitegws@gmail.com>
2017-04-14 09:14:11 +0800

    add binarysearch

--- src/algorithm/BinarySearch.java
+++ src/algorithm/BinarySearch.java
@@ -9,7 +9,27 @@ public class BinarySearch {
 
 	public static int binarySearch(int[] nums, int target){
 
+		if (nums.length == 0 ){
+			return -1;
+		}
 		
+		int low = 0;
+		int high = nums.length - 1;
+		
+		while (low < high){
+			int middle = low + (high - low) / 2;
+			if (nums[middle] == target){
+				high = middle;
+			} else if (nums[middle] > target){
+				high = middle - 1;
+			} else {
+				low = middle + 1;
+			}
+		}
+		
+		if (nums[high] == target){
+			return high;
+		}
 		return -1;
 	}
 }
