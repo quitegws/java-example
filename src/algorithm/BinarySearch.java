dae940e8a2e14c8e0b8c41fff770efb9fa9b04ad
quitegws <quitegws@gmail.com>
2017-04-14 09:03:20 +0800

    add one solution

---
+++ src/algorithm/BinarySearch.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static int binarySearch(int[] nums, int target){
+
+		
+		return -1;
+	}
+}
