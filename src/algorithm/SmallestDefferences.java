e7d21ba7c1d33a06220a45daa45d047c642ebdda
quitegws <quitegws@gmail.com>
2017-04-17 22:45:49 +0800

    add solution

---
+++ src/algorithm/SmallestDefferences.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class SmallestDefferences {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    /**
+     * @param A, B: Two integer arrays.
+     * @return: Their smallest difference.
+     */
+    public int smallestDifference(int[] A, int[] B) {
+        // write your code here
+    }
+
+}
